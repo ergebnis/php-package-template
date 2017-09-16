e902283a7ccf14d951e4b6359ecbf2e877d47099
Andreas Möller <am@localheinz.com>
2017-09-16 18:23:51 +0200

    Enhancement: Add project code test

---
+++ test/Unit/ProjectCodeTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2017 Andreas Möller.
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @link https://github.com/localheinz/repository
+ */
+
+namespace Localheinz\Repository\Test\Unit;
+
+use Localheinz\Test\Util\Helper;
+use PHPUnit\Framework;
+
+final class ProjectCodeTest extends Framework\TestCase
+{
+    use Helper;
+
+    public function testProductionClassesAreAbstractOrFinal()
+    {
+        $this->assertClassesAreAbstractOrFinal(__DIR__ . '/../../src');
+    }
+
+    public function testProductionClassesHaveTests()
+    {
+        $this->assertClassesHaveTests(
+            __DIR__ . '/../../src',
+            'Localheinz\\Repository\\',
+            'Localheinz\\Repository\\Test\\Unit\\'
+        );
+    }
+
+    public function testTestClassesAreAbstractOrFinal()
+    {
+        $this->assertClassesAreAbstractOrFinal(__DIR__ . '/..');
+    }
+}
