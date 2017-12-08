a5a0c49a5268d425d82ae666bfbb9acb03e34dee
Andreas Möller <am@localheinz.com>
2017-12-09 00:21:10 +0100

    Fix: Run 'make cs'

--- test/Unit/ProjectCodeTest.php
+++ test/Unit/ProjectCodeTest.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * For the full copyright and license information, please view
  * the LICENSE file that was distributed with this source code.
  *
- * @link https://github.com/localheinz/repository
+ * @see https://github.com/localheinz/repository
  */
 
 namespace Localheinz\Repository\Test\Unit;
