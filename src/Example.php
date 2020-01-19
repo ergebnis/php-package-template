21dbc2fd7774987bdf28326db37499206a2987f3
Andreas Möller <am@localheinz.com>
2020-01-19 14:59:07 +0100

    Fix: Run 'make coding-standards'

--- src/Example.php
+++ src/Example.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
  * Copyright (c) 2019-2020 Andreas Möller
  *
  * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
+ * the LICENSE.md file that was distributed with this source code.
  *
  * @see https://github.com/ergebnis/php-library-template
  */
