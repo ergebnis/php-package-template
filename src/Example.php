145f95a97c49289290d3c24b1e07c07b849e0ab4
Andreas Möller <am@localheinz.com>
2023-02-16 23:39:14 +0100

    Enhancement: Use constructor property promotion

--- src/Example.php
+++ src/Example.php
@@ -15,11 +15,8 @@ namespace Ergebnis\Package;
 
 final class Example
 {
-    private string $name;
-
-    private function __construct(string $name)
+    private function __construct(private readonly string $name)
     {
-        $this->name = $name;
     }
 
     public static function fromName(string $name): self
