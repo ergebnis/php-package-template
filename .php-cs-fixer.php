0a68b3724c78f6008d481d5e3700a3bbf4aea8d6
Andreas Möller <am@localheinz.com>
2023-06-21 10:11:53 +0200

    Fix: Remove unnecessary statement

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -35,8 +35,7 @@ $config->getFinder()
         '.note/',
     ])
     ->ignoreDotFiles(false)
-    ->in(__DIR__)
-    ->name('.php-cs-fixer.php');
+    ->in(__DIR__);
 
 $config->setCacheFile(__DIR__ . '/.build/php-cs-fixer/.php-cs-fixer.cache');
 
